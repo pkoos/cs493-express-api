28c20fa89b28c22f4c32e980feb6c44668f7358c
Paul Koos <paully_k5298@yahoo.com>
2023-05-15 23:17:47 -0700

    clean up index file, removing old commented out code. Remove unused imports, and add initializeDatabase function call.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,20 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import mysql2, { Pool } from 'mysql2/promise';
-import { Business, getBusinesses, addNewBusiness, modifyBusiness, removeBusiness, getBusinessDetails } from './models/business';
-import { Review, addReview, modifyReview, removeReview, getReviews } from './models/review';
-import { Photo, addPhoto, getPhotos, modifyPhoto, removePhoto } from './models/photo';
-// import * as rh from './controllers/responses-helper';
+import { getBusinesses, addNewBusiness, modifyBusiness, removeBusiness, getBusinessDetails } from './models/business';
+import { addReview, modifyReview, removeReview, getReviews } from './models/review';
+import { addPhoto, getPhotos, modifyPhoto, removePhoto } from './models/photo';
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 const db:Pool = mysql2.createPool({
     connectionLimit: 10,
     user: "busi-user",
     password: "asdfqwer1234",
     database: "busirate",
-    host: "localhost",
+    host: "localhost", // use the container name
     port: 3306
 });
-
-// let reviews: Review[] = [];
-// let photos: Photo[] = [];
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -62,44 +58,17 @@ app.post(`${modifyPhotoPath}/:id`, (req: Request, res: Response) => modifyPhoto(
 
 const getphotosPath:string = `${baseApiPath}/photos`;
 app.get(getphotosPath, (req: Request, res: Response) => getPhotos(db, req, res));
-// {
-//     if (req.query.ownerId) {
-//         let owned_photos: Photo[] = [];
-//         const owner_id: number = parseInt(String(req.query.ownerId));
-//         photos.forEach( (photo) => {
-//             if(photo.ownerId == owner_id) {
-//                 owned_photos.push(photo);
-//             }
-//         });
-
-//         rh.successResponse(res, {"ownerId": req.query.ownerId, "photos": owned_photos});
-//         return;
-//     }
-    
-//     rh.genericErrorResponse(res, 400, "Missing ownerId query");
-// });
 
 const getReviewsPath: string = `${baseApiPath}/reviews`;
 app.get(getReviewsPath, (req: Request, res: Response) => getReviews(db, req, res));
-// {
-//     if(req.query.ownerId) {
-//         let owned_reviews: Review[] = [];
-//         const owner_id:number = parseInt(String(req.query.ownerId));
-//         reviews.forEach( (review) => {
-//             if(review.ownerId == owner_id) {
-//                 owned_reviews.push(review);
-//             }
-//         });
-
-//         rh.successResponse(res, {"ownerId": req.query.ownerId, "reviews": owned_reviews});
-//         return;
-//     }
-
-//     rh.genericErrorResponse(res, 400, "Missing ownerId query");
-// });
 
 // https://stackoverflow.com/questions/33547583/safe-way-to-extract-property-names
+
+function initializeDatabase() {
+    console.log("Initializing database");
+}
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}.`);
 });
+
